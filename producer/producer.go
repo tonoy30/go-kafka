9ae6b11504ec23318d4f44ec787bc8c3be5cbdfb
tonoy30 <tonoy.sust@gmail.com>
2022-01-09 17:39:28 +0600

    readme

--- producer/producer.go
+++ producer/producer.go
@@ -1,7 +1,38 @@
 package producer
 
-import "github.com/Shopify/sarama"
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
 
 func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
-	return nil, nil
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+	conn, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func PushCommitToTopic(topic string, message []byte) error {
+	brokersUrl := []string{"kafkahost1:9092", "kafkahost2:9092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
 }
